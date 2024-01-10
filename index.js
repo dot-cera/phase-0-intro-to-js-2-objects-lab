87d1684276c37d9bd1574ea7ed47fc31d36b225a
dot-cera <ritaclaris980@gmail.com>
2024-01-10 11:59:05 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+let employee = {name: 'Claries', streetAddress: 'Birmingham'}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updatedEmployee = { ...employee };
+  updatedEmployee[key] = value;
+  return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const deletedKey = { ...employee };
+  delete deletedKey[key];
+  return deletedKey;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  delete employee[key];
+  return employee;
+}
